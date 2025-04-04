d8481f5e894070158f11df544f6430ec831fa7a6
sushanth-shetty11 <11s.shettyy@gmail.com>
2025-04-04 13:35:19 +0530

    Added for loop and switch statement programs

---
+++ control statement/for loop/p24.java
@@ -0,0 +1,17 @@
+class p24
+{
+	public static void main(String[] args)
+	{
+		int n=5; //SIZE OF SQUARE
+
+		for(int i=0; i<n; i++) // NO OF ROWS
+		{
+			for(int j=0; j<n; j++) // NO OF COLS
+			{
+				System.out.print("*"+" ");
+			}
+				System.out.println(); // MOVE TO NEXT / NEW LINE
+			
+		}
+	}
+}
