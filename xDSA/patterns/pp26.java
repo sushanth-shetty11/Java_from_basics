af533ade600382fe8becddbb2656a1af5ae2cf58
sushanth-shetty11 <11s.shettyy@gmail.com>
2025-05-26 09:39:30 +0530

    +1

--- xDSA/patterns/pp26.java
+++ xDSA/patterns/pp26.java
@@ -1,25 +1,16 @@
-public class pp25 {
+public class pp26 {
     public static void main(String[] args) {
         int n=5;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                if( i==j || i+j==n+1){
+                if(i==1 || j==1 || i==n || j==n || i+j==n+1 ){
                     System.out.print("* ");
                 }
-                else{
-                    System.out.print("  ");
+                    else{
+                        System.out.print("  ");
                 }
-            }
+             }
             System.out.println();
         }
-    }
+    }    
 }
-
-
-// ---op---
-
-// *       * 
-//   *   *
-//     *
-//   *   *
-// *       *
