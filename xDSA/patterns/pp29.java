9775cdfd9a69c53cb9050600ebe72380444e8885
sushanth-shetty11 <11s.shettyy@gmail.com>
2025-05-29 09:40:13 +0530

    strings

--- xDSA/patterns/pp29.java
+++ xDSA/patterns/pp29.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
-
 public class pp29 {
-    static void pattern(int n){
-       
-    for(int i=1;i<=n;i++){
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             if(i==1 || j==1 || i==n || j==n && i>=n/2+1 || i==n/2+1 && j>=n/2+1){
                 System.out.print("* ");
@@ -14,19 +12,15 @@ public class pp29 {
         }
         System.out.println();
     }
-    }
-    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("enter the number");
-    int n= sc.nextInt();
-    System.out.println("the op is : "+ pattern(n));
-    }
-   
+  }
 }
+       
+    
+
 
 // ---op---
 
-// * * * * * 
+// * * * * *
 // *
 // *   * * *
 // *       *
