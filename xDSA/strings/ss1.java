e4a95e3a5893993100845d1cf22ab88b8638fea8
sushanth-shetty11 <11s.shettyy@gmail.com>
2025-06-07 22:23:21 +0530

    add

--- xDSA/strings/ss1.java
+++ xDSA/strings/ss1.java
@@ -1,30 +1,33 @@
-// public class ss1 {
-//  public static void main(String[] args) {
-//         String str ="who are you";
-//         StringBuilder sb = new StringBuilder(str);
-//         sb.reverse();
-//         System.out.println(sb.toString());
-//     }   
-// }
+// TO reverse a String 
 
-public class ss1{
-    public static void main(String[] args) {
-        String str = "who are you";
-        char[] ch = str.toCharArray();
-      
-        int i=0, j=ch.length-1;
 
-        while(i<j){
-            char temp = ch[i];
-            ch[i] = ch[j];
-            ch[j] = temp;
-            i++;
-            j++;
-        }
-          System.out.println(ch);
+public class ss1 {
+ public static void main(String[] args) {
+        String str ="who are you";
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        System.out.println(sb.toString());
     }
-
 }
+
+// public class ss1{
+//     public static void main(String[] args) {
+//         String str = "who are you";
+//         char[] ch = str.toCharArray();
+      
+//         int i=0, j=ch.length-1;
+
+//         while(i<j){
+//             char temp = ch[i];
+//             ch[i] = ch[j];
+//             ch[j] = temp;
+//             i++;
+//             j++;
+//         }
+//           System.out.println(ch);
+//     }
+
+// }
 
 // ---or---
 
@@ -39,6 +42,20 @@ public class ss1{
 //         System.out.println(rev);
 //     }
 // }
+
+// ---or---
+
+// public class ss1{
+//     public static void main(String[] args) {
+//         String str = "who are you";
+//         char[] ch = str.toCharArray();
+//         for(int i=ch.length-1;i>=0;i--){
+//             System.out.print(ch[i]);
+//         }
+//     }
+// }
+
+// in this method, it does not store in string it just displays it
 
 
 
