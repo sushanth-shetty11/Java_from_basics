3b33ad6774aff457d92b1b5b5f1fb090fda8004b
sushanth-shetty11 <11s.shettyy@gmail.com>
2025-06-13 09:07:25 +0530

    arrays

--- xDSA/arrays/aa3.java
+++ xDSA/arrays/aa3.java
@@ -1,3 +1,12 @@
 public class aa3 {
-    psvm
+    public static void main(String[] args) {
+        int a[]= {5,10,15,20};
+        int smallest = a[0];
+        for(int i=1;i<=a.length-1;i++){
+            if(smallest>a[i]){
+                smallest=a[i];
+            }
+        }
+        System.out.println(smallest);
+    }
 }
