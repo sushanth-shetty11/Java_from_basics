456c1422d29198f7c533e55a954a6cb4aa5072d8
sushanth-shetty11 <11s.shettyy@gmail.com>
2025-06-13 15:30:50 +0530

    arrays

--- xDSA/arrays/aa4.java
+++ xDSA/arrays/aa4.java
@@ -1,40 +1,26 @@
 // WAP tp print all the prime numbers 
 
-public class aa4 {
+public class aa4{
     public static void main(String[] args) {
-        int a[]={10,15,17,20};
-        int n=a.length;
-        for(int i=2;i<=n;i++){
-            if(n%i==0){
-                System.out.println("");
-            }
-            
+        int a[] = {6,10,17,20,25,19};
+        for(int i=0;i<=a.length-1;i++){
+             if(isPrime(a[i])){
+            System.out.println(a[i]);
         }
-        
+    }
+  }
+    static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
     }
 }
-    
-                
- 
 
-// public class aa4{
-//     public static void main(String[] args) {
-//         int a[] = {6,10,17,20,25,19};
-//         for(int i=0;i<=a.length-1;i++){
-//              if(isPrime(a[i])){
-//             System.out.println(a[i]);
-//         }
-//     }
-//   }
-//     static boolean isPrime(int n){
-//         if(n<=1){
-//             return false;
-//         }
-//         for(int i=2;i<=n/2;i++){
-//             if(n%i==0){
-//                 return false;
-//             }
-//         }
-//         return true;
-//     }
-// }
+// INPUT  = {6,10,17,20,25,19}
+// OUTPUT = 17, 19
