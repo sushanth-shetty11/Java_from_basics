873bac50739bb9f560650567af7735b9186478d3
sushanth-shetty11 <11s.shettyy@gmail.com>
2025-06-23 23:00:10 +0530

    number system

---
+++ xDSA/number_system/n1.java
@@ -0,0 +1,41 @@
+//WAP to find the biggest number among three number using if statement only 
+
+// public class n1 {
+//     public static void main(String[] args) {
+//         int n1=50;
+//         int n2=200;
+//         int n3=3000;
+
+//         if(n1>n2 && n1>n3){
+//             System.out.println(n1);
+//         }
+//         if(n2>n1 && n2>n3){
+//             System.out.println(n2);
+//         }
+//         if(n3>n1 && n3>n2){
+//             System.out.println(n3);
+//         }
+//     }
+// }
+
+//     OR
+
+ public class n1{
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int c=100;
+        int big =a;
+
+        if(b>big){
+            big=b;
+        }
+        if(c>big){
+            big=c;
+        }
+        System.out.println(big);
+    }
+ }
+
+ 
+
