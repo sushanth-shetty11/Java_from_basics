515419d775a554ccc976db55744ad33c9527de72
sushanth-shetty11 <11s.shettyy@gmail.com>
2025-06-23 23:34:25 +0530

    number system

---
+++ xDSA/number_system/n4.java
@@ -0,0 +1,12 @@
+//WAP to check the given number is even or odd without modulus operator
+
+public class n4 {
+    public static void main(String[] args) {
+        int n=7;
+        if(n/2*2==n){
+            System.out.println("even");
+        }else{
+            System.out.println("out");
+        }
+    }
+}
