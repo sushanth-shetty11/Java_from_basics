13ed8850dba710b056cba567632de88d6de23cf5
sushanth-shetty11 <11s.shettyy@gmail.com>
2025-07-02 09:31:17 +0530

    ns

--- xDSA/number_system/n19.java
+++ xDSA/number_system/n19.java
@@ -3,9 +3,29 @@
 public class n19 {
     public static void main(String[] args) {
         int n=145;
+        if(isStrong(n)) {
+            System.out.println("Strong number");
+        }else{
+            System.out.println("Not an Strong number");
+        }
     }
-    static int isFactorial(int n){
-        int t
+    static int sumOfFact(int n){
+        int sum=0;
+        while(n>0){
+           int rem=n%10;
+           int fact=1;
+           for(int i=1;i<=rem;i++){
+            fact=fact*i;
+           }
+           sum+=fact;
+           n=n/10;
+        }
+        return sum;
     }
-
+    static boolean isStrong(int n){
+      return sumOfFact(n)==n;
+    }
 }
+
+
+//OUTPUT - Strong number
